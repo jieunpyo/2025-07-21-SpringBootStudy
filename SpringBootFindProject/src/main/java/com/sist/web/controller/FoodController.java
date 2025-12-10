f48911d1a365f28b8f43d3d24d9308d95862a2f9
jieunpyo <jieunhye1121@naver.com>
2025-12-10 12:00:17 +0900

    1111

--- SpringBootFindProject/src/main/java/com/sist/web/controller/FoodController.java
+++ SpringBootFindProject/src/main/java/com/sist/web/controller/FoodController.java
@@ -1,7 +1,9 @@
 package com.sist.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // router => 화면 변경
 @Controller
@@ -10,5 +12,11 @@ public class FoodController {
   public String food_find()
   {
 	  return "food/find"; // templates/food/find.html
+  }
+  @GetMapping("/food/detail")
+  public String food_detail(@RequestParam("fno") int fno,Model model)
+  {
+	  model.addAttribute("fno", fno);
+	  return "food/detail";
   }
 }
